d92b59677d8293b38148f29c237c9a7eea25bd1b
kumar6646 <sai.penusoft@gmail.com>
2025-08-08 10:49:52 +0530

    comments wrote

--- ConsoleAPpFirst/Program.cs
+++ ConsoleAPpFirst/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleAPpFirst
     {
         static void Main(string[] args)
         {
+            //Comments are of two types:
+            //1)Single Line Comment :
+            //2)Multi line comment 
+            /*
+             * Comments are  not
+             * read by the compiler
+             * it will exclude those 
+             * comments
+             */
             Console.WriteLine("Hello World");
             Console.WriteLine("Added new line");
             Console.WriteLine("Made changes");
